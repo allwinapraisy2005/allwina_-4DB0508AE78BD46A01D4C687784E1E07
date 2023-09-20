a762ab67fa234e722ff166b66563435ddf9aa3eb
allwinapraisy2005 <144765014+allwinapraisy2005@users.noreply.github.com>
2023-09-20 09:55:03 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,8 +1,32 @@
-# Get input from user
-year = int(input("Enter a year: "))
-# Check if it's a leap year
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(f"{year} is a leap year.")
-else:
-    print(f"{year} is not a leap year.")
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return f"Deposited ${amount}. New balance: ${self.__account_balance}"
+        else:
+            return "Invalid deposit amount. Amount must be greater than 0."
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.__account_balance}"
+        else:
+            return "Invalid withdrawal amount or insufficient balance."
+
+    def display_balance(self):
+        return f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}"
+
+
+# Creating an instance of BankAccount
+account = BankAccount("1234567890", "John Doe", 1000.0)
+
+# Testing deposit and withdrawal functionality
+print(account.display_balance())
+print(account.deposit(500))
+print(account.withdraw(200))
+print(account.withdraw(1500))  # This should result in an error message
